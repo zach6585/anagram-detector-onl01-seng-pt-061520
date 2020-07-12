bf6b16a3ea362e7be1a426c340e44aaf96005556
zachary mines <zmines@wesleyan.edu>
2020-07-12 20:43:02 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram 
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(wards)
+    a = []
+    wards.each do |ward|
+      if word.split("").sort == ward.split("").sort 
+        a << ward
+      end 
+    end 
+    return a
+  end 
+  
+end 
